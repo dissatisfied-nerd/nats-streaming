d19220d96df361bc0d85dc02c9d837e7870fd74f
dissatisfied-nerd <sanek.glov@gmail.com>
2023-10-21 01:55:52 +0300

    git diff fix

--- server/go.mod
+++
@@ -1,8 +0,0 @@
-module server
-
-go 1.21.2
-
-require (
-	github.com/dissatisfied-nerd/nats-streaming/pkg/dbcontroller v0.0.0-20231020223509-414c34d66a5e // indirect
-	github.com/jmoiron/sqlx v1.3.5 // indirect
-)
--- server/main.go
+++ server/main.go
@@ -1,7 +1,12 @@
 package main
 
-import dbc "github.com/dissatisfied-nerd/nats-streaming/pkg/dbcontroller"
+import (
+	"os"
+
+	dbc "github.com/dissatisfied-nerd/nats-streaming/pkg/dbcontroller"
+)
 
 func main() {
-	dbc.NewDbclient()
+	dbURL := os.Getenv("POSTGRES_URL")
+	dbc.NewDbclient(dbURL)
 }
