7a2447f1f470a9e31623161fa37bacef708d9f41
dissatisfied-nerd <sanek.glov@gmail.com>
2023-10-21 01:23:26 +0300

    server schedule

--- server/dbcontroller/dbcontroller.go
+++ server/dbcontroller/dbcontroller.go
@@ -1,1 +1,20 @@
 package dbcontroller
+
+import (
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func NewDbclient(*sqlx.Conn, error) {
+	var DbURL = os.Getenv("POSTGRES_URL")
+
+	db, err := sqlx.Connect("postgres", DbURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.Ping()
+}
