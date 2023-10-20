75b13cc9bfd117b0c45243163066da8b2606361b
dissatisfied-nerd <sanek.glov@gmail.com>
2023-10-20 03:25:51 +0300

    Trying to make it clean

---
+++ pkg/checkerror/checkerror.go
@@ -0,0 +1,9 @@
+package checkerror
+
+import "log"
+
+func CheckErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- publisher/main.go
+++ publisher/main.go
@@ -1,4 +1,4 @@
-package publisher
+package main
 
 import (
 	"encoding/json"
@@ -6,9 +6,28 @@ import (
 	"os"
 	"time"
 
-	cherr "github.com/dissatisfied-nerd/nats-streaming/pkg/checkerror"
-	utl "github.com/dissatisfied-nerd/nats-streaming/pkg/utils"
+	"github.com/dissatisfied-nerd/nats-streaming/pkg/model"
+	"github.com/nats-io/stan.go"
 )
+
+func NSConnect(nsUrl, nsCluster, nsClient string) stan.Conn {
+	connection, err := stan.Connect(nsCluster, nsClient, stan.NatsURL(nsUrl))
+	cherr.CheckErr(err)
+
+	return connection
+}
+
+func ParseOrders(dataPath string) model.Order {
+	file, err := os.ReadFile(dataPath)
+	cherr.CheckErr(err)
+
+	var orders model.Order
+
+	err = json.Unmarshal(file, &orders)
+	cherr.CheckErr(err)
+
+	return orders
+}
 
 func main() {
 	dataPath := os.Getenv("PUBLISHER_DATA_PATH")
